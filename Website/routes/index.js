5a844ffd8b0688d722374dffff376d7f34a01ebd
Andy Almeida <andyalmeida191@gmail.com>
2024-09-26 07:00:47 +0000

    Added variables to GetAQuote Render

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -5,12 +5,12 @@ var HeroPhoneBasic = 'Call: 1(949)836-6193'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contents/index', { herohead: 'World-Class Service Since 1984', herosub: HeroPhoneBasic, heroimg: './images/FirstLogo.png' });
+  res.render('contents/index', { herohead: 'World-Class Service Since 1984p', herosub: HeroPhoneBasic, heroimg: './images/heros/sunsetfour.jpg' });
 });
 
 /* GET GetAQuote. */
 router.get('/GetAQuote', function(req, res, next) {
-  res.render('contents/getaquote', { title: 'Express' });
+  res.render('contents/getaquote', { herohead: 'Get in contact with us!', herosub: HeroPhoneBasic, heroimg: './images/heros/onthephone.jpg' });
 });
 
 /* POST GetAQuote. */
