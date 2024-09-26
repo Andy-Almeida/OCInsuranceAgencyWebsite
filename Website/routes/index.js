053c59649b3d198533b141760292901412d51bc3
Andy Almeida <andyalmeida191@gmail.com>
2024-09-26 00:25:26 +0000

    Solid index built. Form htlm and css built

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var HeroPhoneBasic = 'Call: 1(949)836-6193'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('contents/index', { herohead: 'World-Class Service Since 1984', herosub: HeroPhoneBasic, heroimg: './images/FirstLogo.png' });
 });
 
 /* GET GetAQuote. */
 router.get('/GetAQuote', function(req, res, next) {
-  res.render('getaquote', { title: 'Express' });
+  res.render('contents/getaquote', { title: 'Express' });
 });
 
 /* POST GetAQuote. */
