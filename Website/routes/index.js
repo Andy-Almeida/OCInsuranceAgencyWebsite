e15c5d6154a9fcdc847dae13458797cd227ea196
Andy Almeida <andyalmeida191@gmail.com>
2024-09-26 19:08:04 +0000

    Routed all possible paths

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -3,15 +3,27 @@ var router = express.Router();
 
 var HeroPhoneBasic = 'Call: 1(949)836-6193'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('contents/index', { herohead: 'World-Class Service Since 1984p', herosub: HeroPhoneBasic, heroimg: './images/heros/sunsetfour.jpg' });
-});
+function GetFunc(path, view, herohead, herosub, heroimg) {
+  router.get(path, function(req, res, next) {
+    res.render(view, { herohead: herohead, herosub: herosub, heroimg: heroimg });
+  });
+}
 
-/* GET GetAQuote. */
-router.get('/GetAQuote', function(req, res, next) {
-  res.render('contents/getaquote', { herohead: 'Get in contact with us!', herosub: HeroPhoneBasic, heroimg: './images/heros/onthephone.jpg' });
-});
+GetFunc('/', 'contents/index', 'World-Class Service Since 1984', HeroPhoneBasic, './images/heros/sunsetfour.jpg');
+GetFunc('/GetAQuote', 'contents/getaquote', 'Get in contact with us!', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/GeneralLiability', 'contents/index', 'General Liability', HeroPhoneBasic, './images/heros/coffeehands.jpg');
+GetFunc('/Automotive', 'contents/index', 'Automotive', HeroPhoneBasic, './images/heros/desertcarman.jpg');
+GetFunc('/CommercialVehicles', 'contents/index', 'Commercial Vehicles', HeroPhoneBasic, './images/heros/icecreamtruckman.jpg');
+GetFunc('/Home', 'contents/index', 'Home', HeroPhoneBasic, './images/heros/homefall.jpg');
+GetFunc('/Motorcycle', 'contents/index', 'Motorcycle', HeroPhoneBasic, './images/heros/motorcycleview.jpg');
+GetFunc('/OurCompany', 'contents/index', 'Our Company', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/OurTeam', 'contents/index', 'Our Team', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/CustomerReviews', 'contents/index', 'Customer Reviews', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/Espanol', 'contents/index', 'Español', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/KnowledgeBase', 'contents/index', 'Knowledge Base', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/InsuranceFAQs', 'contents/index', 'Insurance FAQs', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/HelpfulLinks', 'contents/index', 'Helpful Links', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/Glossary', 'contents/index', 'Glossary', HeroPhoneBasic, './images/heros/onthephone.jpg');
 
 /* POST GetAQuote. */
 router.post('/UserQuoteSubmitted', function(req, res, next) {
