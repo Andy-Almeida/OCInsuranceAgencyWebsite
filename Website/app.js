43f37fcfb8e45d9a49cc26f1432540e0e36033a9
Andy Almeida <andyalmeida191@gmail.com>
2024-10-06 22:30:33 +0000

    Router Paths modified. Properly used logic

--- Website/app.js
+++ Website/app.js
@@ -2,10 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var formRouter = require('./routes/email');
 
 var app = express();
 
@@ -17,34 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-function RoutingPaths(paths) {
-  paths.forEach(path => {
-    app.use(path, indexRouter);
-  });
-}
-
-var paths = [
-  '/',
-  '/GetAQuote',
-  '/GeneralLiability',
-  '/UserQuoteSubmitted',
-  '/Automotive',
-  '/CommercialVehicles',
-  '/Home',
-  '/Motorcycle',
-  '/OurCompany',
-  '/OurTeam',
-  '/CustomerReviews',
-  '/Espanol',
-  '/KnowledgeBase',
-  '/InsuranceFAQs',
-  '/HelpfulLinks',
-  '/Glossary'
-]
-
-RoutingPaths(paths);
+// Routing Paths
+app.use('/', indexRouter);
+app.use('/', formRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
