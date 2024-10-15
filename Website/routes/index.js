8633ae34c267e167c2e408bf7cc7b4e21dc74e25
Andy Almeida <andyalmeida191@gmail.com>
2024-10-15 01:46:48 +0000

    adding the routing for the links

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -12,17 +12,17 @@ function GetFunc(path, view, herohead, herosub, heroimg) {
 GetFunc('/', 'contents/index', 'World-Class Service Since 1984', HeroPhoneBasic, './images/heros/sunsetfour.jpg');
 GetFunc('/GetAQuote', 'contents/getaquote', 'Get in contact with us!', HeroPhoneBasic, './images/heros/onthephone.jpg');
 GetFunc('/GeneralLiability', 'contents/generalliability', 'General Liability', HeroPhoneBasic, './images/heros/coffeehands.jpg');
-GetFunc('/Automotive', 'contents/automotive', 'Automotive', HeroPhoneBasic, './images/heros/desertcarman.jpg');
-GetFunc('/CommercialVehicles', 'contents/commercialvehicles', 'Commercial Vehicles', HeroPhoneBasic, './images/heros/icecreamtruckman.jpg');
-GetFunc('/Home', 'contents/home', 'Home', HeroPhoneBasic, './images/heros/homefall.jpg');
+GetFunc('/Automotive', 'contents/automotive', 'Automotive', HeroPhoneBasic, './images/heros/carladysmile.jpg');
+GetFunc('/CommercialVehicles', 'contents/commercialvehicles', 'Commercial Vehicles', HeroPhoneBasic, './images/heros/commercialtruck.jpg');
+GetFunc('/Home', 'contents/home', 'Home', HeroPhoneBasic, './images/heros/house.jpg');
 GetFunc('/Motorcycle', 'contents/motorcycle', 'Motorcycle', HeroPhoneBasic, './images/heros/motorcycleview.jpg');
 GetFunc('/OurCompany', 'contents/ourcompany', 'Our Company', HeroPhoneBasic, './images/heros/twopeoplelaptop.jpg');
 GetFunc('/OurTeam', 'contents/ourteam', 'Our Team', HeroPhoneBasic, './images/heros/theteam.jpg');
 GetFunc('/CustomerReviews', 'contents/customerreviews', 'Customer Reviews', HeroPhoneBasic, './images/heros/handshake.jpg');
-GetFunc('/Espanol', 'contents/espanol', 'Español', HeroPhoneBasic, './images/heros/onthephone.jpg');
-GetFunc('/KnowledgeBase', 'contents/knowledgebase', 'Knowledge Base', HeroPhoneBasic, './images/heros/onthephone.jpg');
-GetFunc('/InsuranceFAQs', 'contents/index', 'Insurance FAQs', HeroPhoneBasic, './images/heros/onthephone.jpg');
-GetFunc('/HelpfulLinks', 'contents/helpfullinks', 'Helpful Links', HeroPhoneBasic, './images/heros/onthephone.jpg');
-GetFunc('/Glossary', 'contents/index', 'Glossary', HeroPhoneBasic, './images/heros/onthephone.jpg');
+GetFunc('/Espanol', 'contents/espanol', 'Español', HeroPhoneBasic, './images/heros/womancafe.jpg');
+GetFunc('/KnowledgeBase', 'contents/knowledgebase', 'Knowledge Base', HeroPhoneBasic, './images/heros/books.jpg');
+GetFunc('/InsuranceFAQs', 'contents/index', 'Insurance FAQs', HeroPhoneBasic, './images/heros/question.jpg');
+GetFunc('/HelpfulLinks', 'contents/helpfullinks', 'Helpful Links', HeroPhoneBasic, './images/heros/lasigns.jpg');
+GetFunc('/Glossary', 'contents/index', 'Glossary', HeroPhoneBasic, './images/heros/openbooks.jpg');
 
 module.exports = router;
